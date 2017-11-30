d33377bd043071b7ca75b3b56d2053ea8c85367e
Cosmos-it <cosmostaban@gmail.com>
2017-11-29 16:37:04 -0800

    Updates

--- StandardWebApi/FrontEndScripts/front-end-script.js
+++ StandardWebApi/FrontEndScripts/front-end-script.js
@@ -16,6 +16,7 @@ function GetProducts() {
     self.singleItem = ko.observable();
     self.search = ko.observable();
 
+    /** get all data **/
     $.getJSON('api/products', function (allData) {
         var mappedProducts = $.map(allData, function (item) { return new Product(item) });
         self.products(mappedProducts);
@@ -23,38 +24,42 @@ function GetProducts() {
 
     // Operations
     self.addData = function () {
-        var id = 0;
-        var lastItem = self.products()[self.products().length - 1];
-        console.log(lastItem.Id());
+        /** get the last product value **/
+        var lastItemId = self.products()[self.products().length - 1];
 
-        //set the data to the current data.
+        /** set the data to the current data. **/
         var data = {
-            Id: lastItem.Id() + 1,
+            Id: lastItemId.Id() + 1, //Add 1 to the last id
             Name: self.Name(),
             Category: self.Category(),
             Price: self.Price()
         };
+
+        /** save it to local array observable **/
         self.products.push(new Product(data));
         self.save(data);
 
-        //Clear the data
+        /** Clear the data **/
         self.Category("");
         self.Name("")
         self.Price("")
     };
-    //deletes from the server
-    self.delete = function (id) {
-
-    }
-
-    //removes from the front end
+    /**
+    removes from the front end first.
+     This will allow the user to think before 
+     Going ahead with this process to avoid data removal.
+    **/
     self.remove = function (dataObj) {
         var id = dataObj.Id();
         self.products.destroy(dataObj);
+        self.delete(id);
+    }
 
+    //deletes from the server
+    self.delete = function (id) {
         alert("Are you sure about this?");
-        self.ajaxCalls('api/products/'+id, 'DELETE').done(function (data) {
-            $('#success').text("Deleted: " + formatItem(data));
+        self.ajaxCalls('api/products/' + id, 'DELETE').done(function (data) {
+            $('#success').text("Successfully deleted: " + formatItem(data));
         });
     }
 
@@ -71,10 +76,6 @@ function GetProducts() {
         self.ajaxCalls(uri, 'POST', data);
     }
 
-    function formatItem(item) {
-        return item.Name + ': $' + item.Price;
-    }
-
     //General ajax call
     self.ajaxCalls = function (uri, method, data) {
         return $.ajax({
@@ -88,9 +89,13 @@ function GetProducts() {
             $('#product').text('Error: ' + errorThrown);
         });
     }
+
+    function formatItem(item) {
+        return item.Name + ': $' + item.Price;
+    }
 }
 
 $(document).ready(function () {
-
+    /** do binding **/
     ko.applyBindings(new GetProducts());
 });
